5f9b4026ffcfa0f5cdd4f64aec2937e1c3d39032
Jacob <jacob.bouchard@elucidia.com>
2020-04-23 20:10:28 -0400

    changed ice servers

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -5,7 +5,7 @@ import * as copy from 'copy-to-clipboard';
 import {RoomService} from '../services/room.service';
 
 const ICE_SERVERS = [
-  {urls: 'stun:stun.l.google.com:19302'}
+  {url: 'stun:stun.l.google.com:19302'}
 ];
 
 @Component({
@@ -101,7 +101,21 @@ export class RoomComponent implements OnInit {
       return;
     }
 
-    const peer = new RTCPeerConnection({iceServers: ICE_SERVERS});
+    const peer = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: [
+            'turn:173.194.72.127:19305?transport=udp',
+            'turn:[2404:6800:4008:C01::7F]:19305?transport=udp',
+            'turn:173.194.72.127:443?transport=tcp',
+            'turn:[2404:6800:4008:C01::7F]:443?transport=tcp'
+          ],
+          username: 'CKjCuLwFEgahxNRjuTAYzc/s6OMT',
+          credential: 'u1SQDR/SQsPQIxXNWQT7czc/G4c='
+        },
+        {urls: ['stun:stun.l.google.com:19302']}
+      ]
+    });
 
     this.peers[peerId] = peer;
 
